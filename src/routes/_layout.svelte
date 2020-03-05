4abe8970c98ccab9e3770f60af9f4219e91c049b
Andrew Molyuk <andrew@molyuk.com>
2020-03-05 18:23:17 +0200

    Add prettier and scss support

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,34 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+<style lang="scss">
+  @import "../global.scss";
+
+  main {
+    position: relative;
+    max-width: 56em;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-2324643-1">
+
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "UA-2324643-1");
+  </script>
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,57 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<style lang="scss">
+  @import "../global.scss";
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  h1 {
+    font-size: 1.8em;
+    font-weight: 400;
+    margin: 0 0 0.5em 0;
+    color: $color;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  img {
+    width: 100%;
+    max-width: 128px;
+    margin: 0 0 1em 0;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 3em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Andrew Molyuk</title>
+  <script
+    src="https://kit.fontawesome.com/76b83863e9.js"
+    crossorigin="anonymous">
+
+  </script>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Andrew Molyuk</h1>
 
 <figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
+  <img alt="Andrew Molyuk" src="avatar-128.png" />
+  <p>
+    I'm father of four, happy husband, hands-on development manager, experienced
+    software architect, principal coder, skilled devops and technology guru. I’m
+    not only used to wearing many hats, I sincerely enjoy it.
+  </p>
 </figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
