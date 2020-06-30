47af8e6ee0666bf75ece3f78be66ddc7469cafcc
Andrew Molyuk <andrew@molyuk.com>
2020-06-30 14:00:32 +0300

    Add aliases

--- tf/route53.tf
+++ tf/route53.tf
@@ -52,6 +52,30 @@ resource "aws_route53_record" "mail" {
   ttl     = "900"
 }
 
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "molyuk.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.molyuk.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "web" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "andrew.molyuk.com"
