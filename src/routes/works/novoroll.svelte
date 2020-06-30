5aaec7b8073988d21cab3ea5cc35398e9bc4a83b
Andrew Molyuk <andrew@molyuk.com>
2020-06-30 21:14:45 +0300

    Add images

--- src/routes/works/novoroll.svelte
+++ src/routes/works/novoroll.svelte
@@ -1,8 +1,22 @@
 <style lang="scss" scoped>
-  #cards {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 3em;
+  .slider {
+    font-size: 0;
+    white-space: nowrap;
+    margin: 10px 0 10px 10px;
+    width: 100%;
+    overflow: scroll;
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+
+    img {
+      height: 400px;
+      margin-right: 10px;
+      cursor: pointer;
+    }
+  }
+  .slider::-webkit-scrollbar {
+    width: 0px;
+    background: transparent; /* make scrollbar transparent */
   }
 </style>
 
@@ -11,3 +25,26 @@
 </svelte:head>
 
 <h1>Novoroll</h1>
+
+<div class="slider">
+  <img src="novoroll/1.jpeg" alt="" />
+  <img src="novoroll/2.jpeg" alt="" />
+  <img src="novoroll/3.jpeg" alt="" />
+  <img src="novoroll/4.jpeg" alt="" />
+  <img src="novoroll/5.jpeg" alt="" />
+  <img src="novoroll/7.jpeg" alt="" />
+  <img src="novoroll/15.jpeg" alt="" />
+  <img src="novoroll/8.jpeg" alt="" />
+  <img src="novoroll/16.jpeg" alt="" />
+  <img src="novoroll/9.jpeg" alt="" />
+  <img src="novoroll/10.jpeg" alt="" />
+  <img src="novoroll/11.jpeg" alt="" />
+  <img src="novoroll/20.jpeg" alt="" />
+  <img src="novoroll/12.jpeg" alt="" />
+  <img src="novoroll/13.jpeg" alt="" />
+  <img src="novoroll/17.jpeg" alt="" />
+  <img src="novoroll/14.jpeg" alt="" />
+  <img src="novoroll/18.jpeg" alt="" />
+  <img src="novoroll/19.jpeg" alt="" />
+  <img src="novoroll/6.jpeg" alt="" />
+</div>
