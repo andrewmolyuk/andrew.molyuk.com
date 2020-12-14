375f11121baf5c591a5607ab3dcb5208e7bdeb62
Andrew Molyuk <andrew@molyuk.com>
2020-12-14 14:48:53 +0200

    Move info to external json

--- src/routes/Info.tsx
+++ src/routes/Info.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Grid = styled.div`
@@ -18,6 +18,7 @@ const Title = styled.h1`
     font-size: 3em;
   }
 `;
+
 const Card = styled.div``;
 
 const CardTitle = styled.h2`
@@ -28,95 +29,31 @@ const CardTitle = styled.h2`
   }
 `;
 
+interface ICard {
+  title: string;
+  p1: string;
+  p2: string;
+}
+
 export default function Home() {
+  const [cards, setCards] = useState<ICard[]>([]);
+  useEffect(() => {
+    fetch("/data/info.json")
+      .then((response) => response.json())
+      .then((data) => setCards(data));
+  }, []);
+
   return (
     <div>
       <Title>Hi, nice to meet you</Title>
-
       <Grid>
-        <Card>
-          <CardTitle>I'm Andrew Molyuk</CardTitle>
-          <p>
-            I am the father of a gang of four, a happy husband, a hands-on
-            development manager, an experienced software architect, a principal
-            coder, a skilled devops and technology guru. I’m not only used to
-            wearing those many hats, I sincerely enjoy it.
-          </p>
-          <p>
-            I have many years of experience leading teams and companies to
-            success.
-          </p>
-        </Card>
-        <Card>
-          <CardTitle>1997</CardTitle>
-          <p>
-            I joined a mid level Israeli company as developer in database
-            management team to help shape the data and replication processes.
-          </p>
-          <p>
-            It was a very challenging time to work immediatelly after emigrating
-            to sunny Israel, but it was offset by acquaintance with so many
-            professionals who accompany me to this day.
-          </p>
-        </Card>
-        <Card>
-          <CardTitle>2003</CardTitle>
-          <p>
-            First steps in complex financial systems, telecom solutions and
-            involvement in design and development of huge web platform using
-            data science to improve oncology care in children's departments.
-          </p>
-          <p>
-            I got a very humane experience, we participated in the system
-            deployment and configuration in hospitals and closely communicated
-            with both doctors and children.
-          </p>
-        </Card>
-        <Card>
-          <CardTitle>2008</CardTitle>
-          <p>
-            I've been working as an architect in a small company that was
-            engaged in managing video assets in the web and had huge ambitions.
-            For a few years we became partners with all Israeli media moguls
-            such as the Ynet, Maariv, Walla, TheMarker, Yellow pages and so on.
-          </p>
-          <p>
-            It was a very interesting and challenging time, especially since the
-            whole system was designed and developed from scratch and I got my
-            first C-level managerial expirience.
-          </p>
-        </Card>
-        <Card>
-          <CardTitle>2016</CardTitle>
-          <p>
-            It was a time of American TV shows and very hard work. Distributed
-            development teams and local units took all the time. In very short
-            time we created a sophisticated voting system for an
-            ultra-large-scale application that can handle 30 million
-            simultaneous connections at a speed of up to 3 million requests per
-            second. The system performed in US, Brazil, Argentina, Portugal,
-            Indonesia, Israel and Russia.
-          </p>
-          <p>
-            I was also excited to have collaborated with ABC, Disney, Univision
-            and Fusion.
-          </p>
-        </Card>
-        <Card>
-          <CardTitle>Now</CardTitle>
-          <p>
-            I deeply entered the world of video ads and arbitration. Fully
-            remote development team, about 23 billion aggregated events daily,
-            complicated hiring process, 300 million database entries updated
-            daily, complex decision-making algorithms, more than 250
-            automatically scalable computing units at peak load and effective
-            business processes automation.
-          </p>
-          <p>
-            Coronola time, quarantine, working from home, new challenges and a
-            lot of fun everyday.
-          </p>
-        </Card>
+        {cards.map((card) => (
+          <Card>
+            <CardTitle>{card.title}</CardTitle>
+            <p>{card.p1}</p>
+            <p>{card.p2}</p>
+          </Card>
+        ))}
       </Grid>
     </div>
   );
